20c9ed979bded3b73c9977aae3481e7bc6b13436
muhafara <faranmuhammad@hotmail.com>
2022-01-25 20:00:44 +0000

    Abstract Class Assignment

---
+++ Abstract Class Assignment/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Abstract_Class_Assignment
+{
+    internal class Employee : Person
+    {
+
+        public Employee(string FirstName, string LastName) { 
+                this.FirstName = FirstName;
+                this.LastName = LastName;
+
+}
+        public override void sayName()
+        {
+            Console.WriteLine("Name : " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ Abstract Class Assignment/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    internal abstract class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+
+        public abstract void sayName();
+    }
+}
