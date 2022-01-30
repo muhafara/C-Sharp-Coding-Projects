a0137fcc8135a632cee772d4a5d1bcdb42593406
muhafara <faranmuhammad@hotmail.com>
2022-01-30 20:14:40 +0000

    Display Server Time using dot net Core

---
+++ Display Actual Server Time/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Display_Actual_Server_Time.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
