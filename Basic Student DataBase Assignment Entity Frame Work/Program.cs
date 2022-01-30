a0fc421ae12a23a16f1f3f2b095c523795fda28e
muhafara <faranmuhammad@hotmail.com>
2022-01-30 20:29:56 +0000

    ASP CORE WEBSITE FOR AZURE

---
+++ Basic Student DataBase Assignment Entity Frame Work/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Student_DataBase_Assignment_Entity_Frame_Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext()) {
+                var stud = new Student() { StudentName = "Kyle" };
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+                Console.WriteLine("Succesfuuly added student");
+                Console.Read();
+        }
+        }
+    }
+}
---
+++ Basic Student DataBase Assignment Entity Frame Work/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Student_DataBase_Assignment_Entity_Frame_Work
+{
+    internal class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set;}
+        public DateTime? DateofBirth { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+
+        public Grade Grade { get; set; }
+}
+    }
+
