0193939bcc3d7dcf212c5d6ddfd957f8274dadcb
muhafara <faranmuhammad@hotmail.com>
2022-01-31 13:58:07 +0000

    Generics Assignment

---
+++ Generics Assignment/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+    internal class Employee<T>
+    {
+
+        public Employee() { 
+        this.things = new List<T>();
+        }
+        public List<T> things 
+        {
+            get;
+            set;
+        }    
+    }
+}
---
+++ Generics Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+    internal class Program
+    {  
+        static void Main()
+        {
+
+            Employee<string> employeeName = new Employee<string>();
+            employeeName.things.Add("John");
+            employeeName.things.Add("Justin");
+            employeeName.things.Add("Ben");
+            foreach (var item in employeeName.things)
+            {
+                Console.WriteLine("The List of string is " + item);
+            }
+
+            Console.WriteLine("List of integer");
+
+
+            Employee<int> employeeID = new Employee<int>();
+            employeeID.things.Add(123);
+            employeeID.things.Add(456);
+            employeeID.things.Add(765);
+            foreach (var item in employeeID.things)
+            {
+                Console.WriteLine("The List of Integer is " + item);
+            }
+
+            Console.Read();
+        }
+    }
+}
