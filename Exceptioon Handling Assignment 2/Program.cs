47879d6e6343de5ae2f36c80f4a867d5cd7ab87e
muhafara <faranmuhammad@hotmail.com>
2022-01-31 11:48:53 +0000

    Exceptional Handling Assignment

---
+++ Exceptioon Handling Assignment 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptioon_Handling_Assignment_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int userAge;
+            Console.WriteLine("Enter your Age");
+
+            try
+            {
+                userAge = Convert.ToInt32(Console.ReadLine());
+                if (userAge <= 0)
+                {
+                    Console.WriteLine("Negitive and zero numbers are not allowed to calculate the date of year");
+                }
+                else
+                {
+                userAge = DateTime.Now.Year - userAge;
+                Console.WriteLine("You year of birth is " + userAge.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
