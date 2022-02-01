1b1fa9e2e808ce38bb0f6063571564a1928b3265
muhafara <faranmuhammad@hotmail.com>
2022-02-01 14:59:06 +0000

    Date time Assignment

---
+++ DateTime Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter the number in 24 hour format");
+            int getHour = Convert.ToInt32(Console.ReadLine());
+            int x = getHour + DateTime.Now.Hour;
+
+
+            if (x < 24)
+            {
+                Console.WriteLine("The exact time it will be in "+x + " : " +DateTime.Now.Minute + " : " +DateTime.Now.Second);
+            }
+            else if (x == 24)
+            {
+            Console.WriteLine("The exact time it will be in 00"  + " : " + DateTime.Now.Minute + " : " + DateTime.Now.Second);
+            }
+            else if (x > 24) 
+            {
+            Console.WriteLine("The exact time it will be in " + (x-24) + " : " + DateTime.Now.Minute + " : " + DateTime.Now.Second);
+            }
+            Console.Read();
+        }
+    }
+}
